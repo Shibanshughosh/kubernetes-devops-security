d20f577e8f88cecfb9f84329c5ee2e39587ce5d6
shibanshughosh <shibanshu.ghosh@gmail.com>
2022-04-18 00:01:00 +0530

    log file

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -4,15 +4,19 @@
 #k8s-deployment-rollout-status.sh
 
 sleep 60s
+echo "create deployment log file"
+kubectl describe deploy devsecops -n default >> deploy-failure-${commitId}.txt
+echo "log file created"
 
 if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
 then     
 	echo "Deployment ${deploymentName} Rollout has Failed"
-    kubectl describe deploy devsecops -n default >> deploy-failure-${commitId}.txt
     kubectl -n default rollout undo deploy ${deploymentName}
     exit 1;
 else
 	echo "Deployment ${deploymentName} Rollout is Success"
+    rm deploy-failure-${commitId}.txt
+    echo "log file deleted"
 fi
 ############### k8s-deployment-rollout-status.sh ###############
 
