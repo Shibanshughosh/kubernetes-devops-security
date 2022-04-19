34bed5d289745dc15ca54b1e39da5cb52eaf0b24
shibanshughosh <shibanshu.ghosh@gmail.com>
2022-04-20 02:26:57 +0530

    echo integration test

--- integration-test.sh
+++ integration-test.sh
@@ -14,7 +14,10 @@ if [[ ! -z "$PORT" ]];
 then
 
     response=$(curl -s $applicationURL:$PORT$applicationURI)
+    echo "$response"
+
     http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
+    echo "$http_code"
 
     if [[ "$response" == 100 ]];
         then
